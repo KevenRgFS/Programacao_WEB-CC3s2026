764745404ec2fa789d650d919ad0ebe9ca4f3eca
KevenRgFS <kevenregioFS@gmail.com>
2026-03-19 12:57:22 -0300

    Atv3

---
+++ Atividade3/script.js
@@ -0,0 +1,11 @@
+//Contador
+
+let botao1 = document.getElementById("botao1");
+let resultado = document.getElementById("valor");
+let contagem = 0; 
+
+
+
+botao1.onclick = function(){
+    resultado = contagem;
+}
